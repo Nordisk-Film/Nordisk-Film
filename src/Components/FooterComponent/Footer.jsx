68add762fa9b1f5931b0a4294c1e975d32648c66
SimseBimmer <simonsej1710@gmail.com>
2025-01-22 10:18:22 +0100

    footer progressing

--- src/Components/FooterComponent/Footer.jsx
+++ src/Components/FooterComponent/Footer.jsx
@@ -6,37 +6,41 @@ export const Footer = () => {
       {/* Footer GridContainer */}
       <div className='footerGridContainer'>
         {/* 1. */}
-        <ul>
-          <li>
-            <h3>NORDISK FILM</h3>
-          </li>
-          <li>
-            <p>Øster Uttrupvej 1</p>
-          </li>
-          <li>
-            <p>9000 Aalborg</p>
-          </li>
-        </ul>
+        <div>
+          <h3>Om NF Plakater</h3>
+          <ul>
+            <li><a href="#">Spørgsmål og svar</a></li>
+            <li><a href="#">Om Nordisk Film Biografer</a></li>
+            <li><a href="#">Om Nordisk Film</a></li>
+            <li><a href="#">Biografoverblik</a></li>
+            <li><a href="#">Partnerbiografer</a></li>
+            <li><a href="#">Job i NFBIO</a></li>
+            <li><a href="#">Betalingsbetingelser</a></li>
+            <li><a href="#">Privatlivs- og cookiepolitik</a></li>
+            <li><a href="#">Dansk biograf- og underholdningspanel</a></li>
+            <li><a href="#">Opdatér samtykke for cookies</a></li>
+          </ul>
+        </div>
         {/* 2. */}
-        <ul>
-          <li>
-            <p>CVR: 12345678</p>
-          </li>
-          <li>
-            <p>MAIL: info@wallywood.dk</p>
-          </li>
-          <li>
-            <p>MOBIL: +45 9812 3456</p>
-          </li>
-        </ul>
-        {/* 3.(tom) */}
-        <div></div>
+        <div>
+
+        </div>
+        {/* 3 */}
+        <div>
+          <h3>Business</h3>
+          <ul>
+            <li><a href="#">Gavebilletter</a></li>
+            <li><a href="#">Konferencer og møder</a></li>
+            <li><a href="#">Særforestillinger</a></li>
+            <li><a href="#">Biografreklame</a></li>
+          </ul>
+        </div>
         {/* 4. */}
         <div className='socialMedia'>
-          <a href="pinterest.com"><img src="src/assets/images/Pinterest.svg" alt="Pinterest" /></a>
-          <a href="instagram.com"><img src="src/assets/images/Instagram.svg" alt="Instagram" /></a>
-          <a href="facebook.com"><img src="src/assets/images/Facebook.svg" alt="Facebook" /></a>
-          <a href="x.com"><img src="src/assets/images/Twitter.svg" alt="Twitter" /></a>
+          <a href="pinterest.com"><img src="src/assets/images/PinterestBlue.svg" alt="Pinterest" /></a>
+          <a href="instagram.com"><img src="src/assets/images/instagramBlue.svg" alt="Instagram" /></a>
+          <a href="facebook.com"><img src="src/assets/images/facebookBlue.svg" alt="Facebook" /></a>
+          <a href="x.com"><img src="src/assets/images/twitterBlue.svg" alt="Twitter" /></a>
         </div>
       </div>
     </footer>
