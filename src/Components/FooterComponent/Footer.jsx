98bd98c5b047812bc7b5b4fa93c149123840701b
MaxMadeleine <Max.Madeleine.G@gmail.com>
2025-01-23 08:49:47 +0100

    changed footer to english

--- src/Components/FooterComponent/Footer.jsx
+++ src/Components/FooterComponent/Footer.jsx
@@ -13,59 +13,57 @@ export const Footer = () => {
 
             {/* 1. */}
             <div className='footerGridItemOne'>
-              <h3>Om NF Plakater</h3>
-              <ul>
-                <li><a href="#">Spørgsmål og svar</a></li>
-                <li><a href="#">Om Nordisk Film Biografer</a></li>
-                <li><a href="#">Om Nordisk Film</a></li>
-                <li><a href="#">Biografoverblik</a></li>
-                <li><a href="#">Partnerbiografer</a></li>
-                <li><a href="#">Job i NFBIO</a></li>
-                <li><a href="#">Betalingsbetingelser</a></li>
-                <li><a href="#">Privatlivs- og cookiepolitik</a></li>
-                <li><a href="#">Dansk biograf- og underholdningspanel</a></li>
-                <li><a href="#">Opdatér samtykke for cookies</a></li>
-              </ul>
-            </div>
-            {/* 2. */}
-            <div className='footerGridItemTwo'>
-              <h3>Bioplus</h3>
-              <ul>
-                <li><a href="#">Tilmeld nyhedsbrev</a></li>
-                <li><a href="#">Bliv medlem</a></li>
-                <li><a href="#">Medlemsbetingelser</a></li>
-                <li><a href="#">BIOPLUS UNLIMITED</a></li>
-                <li><a href="#">BIOPLUS og BIOPLUS UNLIMITED FAQ</a></li>
+  <h3>About NF Posters</h3>
+  <ul>
+    <li><a href="#">Questions and answers</a></li>
+    <li><a href="#">About Nordisk Film Cinemas</a></li>
+    <li><a href="#">About Nordisk Film</a></li>
+    <li><a href="#">Poster overview</a></li>
+    <li><a href="#">Partner Posters</a></li>
+    <li><a href="#">Jobs at NF POSTER</a></li>
+    <li><a href="#">Payment terms</a></li>
+    <li><a href="#">Privacy and cookie policy</a></li>
+    <li><a href="#">Danish poster and entertainment panel</a></li>
+    <li><a href="#">Update cookie consent</a></li>
+  </ul>
+</div>
+{/* 2. */}
+<div className='footerGridItemTwo'>
+  <h3>Poster plus</h3>
+  <ul>
+    <li><a href="#">Sign up for the newsletter</a></li>
+    <li><a href="#">Become a member</a></li>
+    <li><a href="#">Membership terms</a></li>
+    <li><a href="#">POSTER PLUS UNLIMITED</a></li>
+    <li><a href="#">POSTER PLUS and POSTER PLUS UNLIMITED FAQ</a></li>
+  </ul>
+</div>
+</div>
+<div className='footerGridItemsWrapperTwo'>
+  {/* 3 */}
+  <div className='footerGridItemThree'>
+    <h3>Business</h3>
+    <ul>
+      <li><a href="#">Gift tickets</a></li>
+      <li><a href="#">Conferences and meetings</a></li>
+      <li><a href="#">Special screenings</a></li>
+      <li><a href="#">Cinema advertising</a></li>
+    </ul>
+  </div>
+  {/* 4. */}
+  <div className='footerGridItemFour'>
+    <h3>Follow us</h3>
+    <div className='socialMedia'>
+      <a href="pinterest.com"><img src="src/assets/images/PinterestBlue.svg" alt="Pinterest" /></a>
+      <a href="instagram.com"><img src="src/assets/images/instagramBlue.svg" alt="Instagram" /></a>
+      <a href="facebook.com"><img src="src/assets/images/facebookBlue.svg" alt="Facebook" /></a>
+      <a href="x.com"><img src="src/assets/images/twitterBlue.svg" alt="Twitter" /></a>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</footer>
 
-              </ul>
-            </div>
-          </div>
-          <div className='footerGridItemsWrapperTwo'>
-
-            {/* 3 */}
-            <div className='footerGridItemThree'>
-              <h3>Business</h3>
-              <ul>
-                <li><a href="#">Gavebilletter</a></li>
-                <li><a href="#">Konferencer og møder</a></li>
-                <li><a href="#">Særforestillinger</a></li>
-                <li><a href="#">Biografreklame</a></li>
-              </ul>
-            </div>
-            {/* 4. */}
-            <div className='footerGridItemFour'>
-              <h3>Følg os</h3>
-              <div className='socialMedia'>
-                <a href="pinterest.com"><img src="src/assets/images/PinterestBlue.svg" alt="Pinterest" /></a>
-                <a href="instagram.com"><img src="src/assets/images/instagramBlue.svg" alt="Instagram" /></a>
-                <a href="facebook.com"><img src="src/assets/images/facebookBlue.svg" alt="Facebook" /></a>
-                <a href="x.com"><img src="src/assets/images/twitterBlue.svg" alt="Twitter" /></a>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </footer>
   )
 }
