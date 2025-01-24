d654820836f4f8cf6b345ac8d6c139a01bc1cb44
MaxMadeleine <Max.Madeleine.G@gmail.com>
2025-01-24 21:03:03 +0100

    whoops forgot to delete import

--- src/Components/RouterComponent/Router.jsx
+++ src/Components/RouterComponent/Router.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "../../PageComponents/HomePageComponent/HomePage";
-import { GenreList } from "../GenreListComponent/GenreList";
 
 
 export const Router = () => {
