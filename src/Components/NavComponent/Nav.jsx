6c2eb0e441150c4b5e0617b9fd8ea25b43231050
SimseBimmer <simonsej1710@gmail.com>
2025-01-28 13:36:39 +0100

    fixed alerts

--- src/Components/NavComponent/Nav.jsx
+++ src/Components/NavComponent/Nav.jsx
@@ -3,21 +3,21 @@ import { NavLink } from "react-router-dom";
 import './Nav.scss'
 
 export const Navbar = () => {
-return (
-    <nav id="nav-section">
-        <ul id="link-left">
-            <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
-            <li><NavLink to="/Posters" activeClassName="active">Posters</NavLink></li>
-            <li><NavLink to="/About" activeClassName="active">About</NavLink></li>
-            <li><NavLink to="/Contact" activeClassName="active">Contact</NavLink></li>
-        </ul>
-        <div id="logo-container">
-            <NavLink to="/"><img src="src/assets/logo/Logo.svg" alt="" /> </NavLink>
-        </div>
-        <ul id="link-right">
-            <li><Link to="/">Log In <img src="src/assets/images/AccountLogo.svg" alt="Account logo" /></Link></li>
-            <li><Link to="/">Basket <img src="src/assets/images/CartLogo.svg" alt="Cart Logo" /></Link></li>
-        </ul>
-    </nav>
-)
+    return (
+        <nav id="nav-section">
+            <ul id="link-left">
+                <li><NavLink to="/" className="active">Home</NavLink></li>
+                <li><NavLink to="/Posters" className="active">Posters</NavLink></li>
+                <li><NavLink to="/About" className="active">About</NavLink></li>
+                <li><NavLink to="/Contact" className="active">Contact</NavLink></li>
+            </ul>
+            <div id="logo-container">
+                <NavLink to="/"><img src="src/assets/logo/Logo.svg" alt="" /> </NavLink>
+            </div>
+            <ul id="link-right">
+                <li><Link to="/">Log In <img src="src/assets/images/AccountLogo.svg" alt="Account logo" /></Link></li>
+                <li><Link to="/">Basket <img src="src/assets/images/CartLogo.svg" alt="Cart Logo" /></Link></li>
+            </ul>
+        </nav>
+    )
 };
