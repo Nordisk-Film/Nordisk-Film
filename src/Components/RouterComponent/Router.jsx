9f1cc3f48cd95e6b91006b295f72c08dc0d6d190
MaxMadeleine <Max.Madeleine.G@gmail.com>
2025-01-29 19:47:06 +0100

    comment for PosterDetail route to clarify use of useParams

--- src/Components/RouterComponent/Router.jsx
+++ src/Components/RouterComponent/Router.jsx
@@ -10,7 +10,7 @@ export const Router = () => {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/posters/:id" element={<PosterDetail />} /> {/* Ensure this route is correct */}
+      <Route path="/posters/:id" element={<PosterDetail />} /> {/* Ville gerne have PosterDetails som et component dog kunne jeg ikke da jeg bruger useparams til at fange id fra url og tror ikke jeg kan bruge useparams fra et component?? */}
       <Route path="/About" element={<AboutPage />} />
       <Route path="/Contact" element={<ContactPage />} />
     </Routes>
