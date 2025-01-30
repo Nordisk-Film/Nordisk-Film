711d480d46492cc19507168f9bee1dd00a76430a
SimseBimmer <simonsej1710@gmail.com>
2025-01-30 09:20:50 +0100

    Cart structure

--- src/Components/CartComponent/Cart.jsx
+++ src/Components/CartComponent/Cart.jsx
@@ -7,15 +7,49 @@ const Cart = ({ isOpen, toggleCart }) => {
         <>
             <div className={`CartOverlay ${isOpen ? 'open' : ''}`} onClick={toggleCart}></div>
             <div className={`Cart ${isOpen ? 'open' : ''}`}>
-                <div className='CartHeader'>
+                <header className='CartHeader'>
                     <div id='CartTitle'>
                         <h2>Shopping cart</h2>
                     </div>
                     <div className='CartCloseContainer' onClick={toggleCart}>
                         <img src={closeIcon} alt="Close" className="CloseCart" />
                     </div>
+                </header>
+                <div className='allProductsContainer'>
+                    {/* product example */}
+                    <div className='productContainer'>
+                        <div>
+                            <img src="src/Components/CartComponent/midlertidlig images/matrixrevolutionsplakat.jpg" alt="product Image" />
+                        </div>
+                        <div className='productText'>
+                            {/* product title */}
+                            <h2 className='productTitle'>The Matrix Revolutions</h2>
+                            <p>100kr</p>
+                            <p className='productHeight'>Height 85 cm</p>
+                            <p className='productWidth'>Width 62 cm</p>
+                            <div>
+                                <div>
+                                    {/* quantity increaser/decreaser nice to have */}
+                                    </div>
+                                    <div>
+                                        <p>remove</p>
+                                    </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <p>Subtotal</p>
+                            </div>
+                            <div>
+                                <p>100,00 kr</p>
+                            </div>
+                                <button className='CheckOut'>
+                                    Checkout
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                {/* Cart items here */}
+
             </div>
         </>
     );
